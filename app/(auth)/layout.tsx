9156910add6983fe8a5250a2e214d685bbbe1e41
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-11-23 20:47:40 +0700

    add assets file

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen w-full">
-      <div className="bg-brand-100 flex h-full w-[40%] flex-col justify-center space-y-20 p-10">
+      <div className="flex h-full w-[40%] flex-col justify-center space-y-20 bg-brand-100 p-10">
         <div className="flex items-center space-x-3">
           <div>
             <Image src="/logo.png" width={50} height={50} alt="" />
@@ -17,8 +17,8 @@ export default function layout({ children }: { children: React.ReactNode }) {
             Awesome, we've created the perfect place for you to store all your documents.
           </p>
         </div>
-        <div className="flex justify-center">
-          <Image src="/auth.png" width={300} height={300} alt="" />
+        <div className="flex justify-center transition-all hover:rotate-12 hover:transform">
+          <Image src="/assets/images/files.png" width={300} height={300} alt="" />
         </div>
       </div>
       <div className="w-[60%]">{children}</div>
