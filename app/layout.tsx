9fe6dc679de62f1ea2c8ec9922fe6502bfd10c2c
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-11-23 16:55:09 +0700

    autoformat setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 import { Poppins } from 'next/font/google';
+
+// https://dbtek.medium.com/add-code-formatting-with-prettier-to-next-js-project-b53c93ffdf91 Auto format setup
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins'
-})
+  variable: '--font-poppins',
+});
 
 export const metadata: Metadata = {
-  title: "StoreIt",
-  description: "Store It - the only solution to store your data.",
+  title: 'StoreIt',
+  description: 'Store It - the only solution to store your data.',
 };
 
 export default function RootLayout({
@@ -20,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} font-poppins antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${poppins.variable} font-poppins antialiased`}>{children}</body>
     </html>
   );
 }
